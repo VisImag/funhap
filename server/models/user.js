1872f70273e83f21d6b6871d6dbc18e4e23edfc2
Akshit Grover <akshit.grover2016@gmail.com>
2019-01-25 16:10:40 +0530

    Server| UserModel: Add checkExistence static function

--- server/models/user.js
+++ server/models/user.js
@@ -9,6 +9,7 @@ const blueprint = new Schema({
   },
   username: {
     type: mongoose.Schema.Types.String,
+    unique: true,
     required: true,
   },
   password: {
@@ -24,5 +25,20 @@ const blueprint = new Schema({
     default: 0,
   },
 });
+
+async function checkExistence(username) {
+  try {
+    const user = await this.findOne({ username });
+    if (user === null || user === undefined) {
+      return false;
+    }
+    return true;
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+}
+
+blueprint.statics.checkExistence = checkExistence;
 
 module.exports = mongoose.model('user', blueprint);
